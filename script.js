4bd801bfd8cc254dce79e1700ae185f5a671d6ba
Gusarich <danielsedovzzz@gmail.com>
2024-04-06 16:55:25 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function updateCountdown() {
+    const countdownDate = new Date("April 6, 2024 20:00:00 UTC").getTime();
+    const now = new Date().getTime();
+    const difference = countdownDate - now;
+
+    if (difference > 0) {
+        const hours = Math.floor(difference / (1000 * 60 * 60));
+        const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+        document.getElementById("timer").innerHTML = hours + "h " + minutes + "m " + seconds + "s";
+    } else {
+        document.getElementById("timer").innerHTML = "The countdown is over!";
+    }
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
