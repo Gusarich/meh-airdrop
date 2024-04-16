aedc411b5c782fd9e454e5a2693c4df92ec6dbb3
Michael <mz-100@mail.ru>
2024-04-16 17:58:44 +0300

    Add landing page

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,13 @@
 import anon from "./anon.webp"
 import meh from "./meh.webp"
+import Logo from "./logo.png"
+import telegram from "./telegram.svg"
+import X from "./X.svg"
 
 export {
+    telegram,
+    X,
+    Logo,
     anon,
     meh,
 }
