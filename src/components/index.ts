0571027a48f32bf212c8873a06f1daa5acfded4b
Michael <mz-100@mail.ru>
2024-05-08 12:13:40 +0300

    Fix Anon staking and and raf staking

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import Main from "./MainPage/Main"
 import Staking from "./staking/Staking"
+import StakingRaf from "./stakingRaf/StakingRaf"
 import ClaimPage from "./claim/claim"
 
 export {
     Main, 
     Staking,
     ClaimPage,
+    StakingRaf,
 }
